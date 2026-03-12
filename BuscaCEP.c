b864a94e6529922f5d7274c9c9cf4e3df3fb149f
Daniel Brito de Souza <danielb.desouza1403@gmail.com>
2026-03-12 01:56:45 -0300

    Refactor cep comparison logic in BuscaCEP.c
    Removed unnecessary comparison and fread call.

--- BuscaCEP.c
+++ BuscaCEP.c
@@ -60,12 +60,6 @@ int main(int argc, char**argv)
 		// argv[1] < e.cep  => strcmp(argv[1],e.cep) < 0
 		// argv[1] > e.cep  => strcmp(argv[1],e.cep) > 0
 		// argv[1] == e.cep  => strcmp(argv[1],e.cep) == 0
-		if(strncmp(argv[1],e.cep,8)==0)
-		{
-			printf("%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
-			break;
-		}
-		qt = fread(&e,sizeof(Endereco),1,f);		
 	}
 	printf("Total Lido: %d\n", c);
 	fclose(f);
